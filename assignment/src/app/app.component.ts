8d9099c2addd587c459ab5070473ff29b64e11d8
Suyash Jaiswal <suyash.ace@gmail.com>
2019-11-20 02:26:40 +0530

    Assignment Completed

--- assignment/src/app/app.component.ts
+++ assignment/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styles: [`
+    .whiteColor {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'assignment';
+  showDetails = true;
+  logs = [];
+
+  toggleLog() {
+    this.showDetails = !this.showDetails;
+    this.logs.push(new Date());
+  }
 }
